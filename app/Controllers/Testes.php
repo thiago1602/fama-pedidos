9e9a83ccb391d068be65ed3626c0bf576f52cbf2
Thiago Moraes <thiagomoraesf95@gmail.com>
2021-09-27 20:31:24 -0300

    first commit

---
+++ app/Controllers/Testes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Testes extends BaseController
+{
+    public function index()
+    {
+        return view('Testes/index');
+    }
+
+    public  function  novo(){
+        echo '';
+    }
+}
