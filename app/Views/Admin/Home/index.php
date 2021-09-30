20d7de1933bfd8b05d578bc2343b0d7ae41ca97c
Thiago Moraes <thiagomoraesf95@gmail.com>
2021-09-29 23:24:01 -0300

    preparando a area restrita(admin)

---
+++ app/Views/Admin/Home/index.php
@@ -0,0 +1,31 @@
+<?php echo $this->extend('Admin/layout/principal');?>
+
+
+<?php echo $this->section('titulo')?> <?php echo $titulo;?> <?php echo $this->endSection()?>
+
+
+<?php echo $this->section('estilos')?>
+
+<!-- Aqui enviamos para o template principal os estilos -->
+
+<?php echo $this->endSection()?>
+
+
+
+
+
+<?php echo $this->section('conteudo')?>
+
+<?php echo $titulo;?>
+
+<?php echo $this->endSection()?>
+
+
+
+
+
+<?php echo $this->section('scripts')?>
+
+<!-- Aqui enviamos para o template principal os scripts -->
+
+<?php echo $this->endSection()?>
